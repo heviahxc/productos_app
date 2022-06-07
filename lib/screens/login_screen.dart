2537544b74b1b6f6ea221eafe0c9fe7b413cfaec
Felipe Hevia <heviamc@gmail.com>
2022-06-07 16:57:07 -0400

    prime

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:productos_app/providers/login_form_provider.dart';
+import 'package:productos_app/ui/input_decoration.dart';
+import 'package:provider/provider.dart';
 
 import '../widgets/widgets.dart';
 
@@ -21,13 +24,21 @@ class LoginScreen extends StatelessWidget {
                     SizedBox(height: 10,),
                     Text('Login', style: Theme.of(context).textTheme.headline4,),
                     SizedBox(height: 30,),
-                    _LoginForm()
+
+
+
+
+                    ChangeNotifierProvider(
+                      create: (_) => LoginFormProvider(),
+                      child: _LoginForm(),
+                      ),
                   ],
                 ),
                 
                 ),
-                SizedBox(height: 50,),
-                Text('Crear una nueva cuenta.', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),)
+                SizedBox(height: 50),
+                Text('Crear una nueva cuenta.', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+                SizedBox(height: 50),
             ],
           ),
         ))
@@ -40,34 +51,76 @@ class _LoginForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    
+  final loginForm  = Provider.of<LoginFormProvider>(context);
     return Container(
            child: Form(
+             key: loginForm.formkey,
+             autovalidateMode: AutovalidateMode.onUserInteraction,
                 child: Column(
                   children: [
                     TextFormField(
                       autocorrect: false,
                       keyboardType: TextInputType.emailAddress,
-                      decoration: const InputDecoration(
-                        enabledBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(
-                            color: Colors.deepPurple
-                          )
+                      decoration: InputDecorations.authInputDecoration(
+                        hintText: 'algo@gmail.com', 
+                        labelText: 'Correo electronico', 
+                        prefixIcon: Icons.alternate_email_sharp
                         ),
-                        focusedBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(
-                            color: Colors.deepPurple,
-                            width: 2
-                          )
-                        ),
-                        hintText: 'heviamc@gmail.com',
-                        labelText: 'Correo electronico',
-                        labelStyle: TextStyle(
-                          color: Colors.grey
-                        ),
-                        prefixIcon: Icon(Icons.alternate_email_sharp, color: Colors.deepPurple,)
+                        onChanged: (value) => loginForm.email = value,
+                        validator: (value){
+                          String pattern = r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+                          RegExp regExp  = new RegExp(pattern);
 
+                          return regExp.hasMatch(value ?? '')
+                          ? null
+                          : 'Debe ser formato correo';
+                        },
+                    ),
+                    SizedBox(height: 30,),
+                     TextFormField(
+                      autocorrect: false,
+                      obscureText: true,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecorations.authInputDecoration(
+                        hintText: '********', 
+                        labelText: 'Contraseña', 
+                        prefixIcon: Icons.lock_outline
                       ),
-                    )
+                      onChanged: (value) => loginForm.password = value,
+                       validator: (value){
+                          
+                          return(value != null && value.length>=6)
+                          ? null
+                          :'La contraseña debe ser de 6 caracteres';
+                        },
+                    ),
+                    SizedBox(height: 30,),
+
+                    MaterialButton(
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                      disabledColor: Colors.grey,
+                      elevation: 0,
+                      color: Colors.deepPurple,
+                      child: Container(
+                        padding: EdgeInsets.symmetric(horizontal: 80, vertical: 15),
+                        child: Text(
+                          loginForm.isLoading
+                          ? 'Espere'
+                          :'Ingresar',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                      onPressed: loginForm.isLoading ? null :()async{
+                        FocusScope.of(context).unfocus();
+
+                      if(!loginForm.isValidForm()) return;
+                      loginForm.isLoading = true;
+
+                    await Future.delayed(Duration (seconds:2));
+                      loginForm.isLoading = false;
+                      Navigator.pushReplacementNamed(context, 'home');
+                    })
                   ],
                 ) ) ,
     );
